c5a6784e84030e3d2299f8048b85d6abfe3fc4b4
Andrey Petrov <andrey.petrov@shazow.net>
2011-10-01 21:01:18 -0700

    Pathfinding bug fix

--- tests/test_intelligence.js
+++ tests/test_intelligence.js
@@ -8,7 +8,7 @@ var get_helpers = function(world, world_box) {
         {x: 0, y: 1},
         {x: 1, y: 0},
         {x: 0, y: -1},
-        {x: -1, y: 1}
+        {x: -1, y: 0}
     ];
 
     return {
@@ -84,13 +84,13 @@ test("pathfind_astar simple", function() {
 
     deepEqual(path, [
         {x: 4, y: 4},
-        {x: 4, y: 3},
-        {x: 4, y: 2},
-        {x: 4, y: 1},
-        {x: 4, y: 0},
-        {x: 3, y: 0},
-        {x: 2, y: 0},
-        {x: 1, y: 0},
+        {x: 3, y: 4},
+        {x: 2, y: 4},
+        {x: 1, y: 4},
+        {x: 0, y: 4},
+        {x: 0, y: 3},
+        {x: 0, y: 2},
+        {x: 0, y: 1},
         {x: 0, y: 0}
     ]);
 
