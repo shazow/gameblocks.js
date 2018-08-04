5b316ce17bb3a2bd3965a5fafaeb07464027f0e7
Andrey Petrov <andrey.petrov@shazow.net>
2018-08-04 14:57:46 -0400

    Convert Types

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,13 +1,3 @@
-class Size2D {
-    width: number;
-    height: number;
-}
-
-class Position2D {
-    x: number;
-    y: number;
-}
-
 interface Camera {
     element: HTMLElement;
     size: Size2D;
